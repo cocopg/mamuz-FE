a902243e7280dde575b2111b171f6b20a18bd853
OTtravel <travelcheck2019@gmail.com>
2026-04-18 17:13:19 +0900

    base commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div>Home</div>
+      <div>Search</div>
+      <div>Cart</div>
+      <div>Profile</div>
+    </div>
+  )
+}
+
+export default Navbar
